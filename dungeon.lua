925c2e01ac0574c7877f224a089640e7d3819210
Oliver Castaneda <olivver@gmail.com>
2019-03-03 17:10:37 -0500

    Adding prefetch

--- dungeon.lua
+++ dungeon.lua
@@ -1,2 +1,42 @@
 USE_CASTLE_CONFIG = true
-require("dungeon_source")
+
+if CASTLE_PREFETCH then
+    
+    if (CASTLE_SERVER) then
+      CASTLE_PREFETCH({
+        'cs.lua',
+        'state.lua',
+        'lib/shash.lua',
+        'lib/list.lua',
+        'lib/sprite',
+        'lib/maze_gen',
+        'moat.lua',
+      })
+    else
+      CASTLE_PREFETCH({
+        'cs.lua',
+        'state.lua',
+        'lib/shash.lua',
+        'lib/list.lua',
+        'lib/sprite.lua',
+        'lib/maze_gen.lua',
+        'moat.lua',
+        'img/wall_top.png',
+        'img/wall_front.png',
+        'img/wall_shadow.png',
+        'img/dirt.png',
+        'img/deep_elf_high_priest.png',
+        'img/conjure_ball_lightning.png',
+        'img/skeleton_bat.png',
+        'img/ogre_mage.png',
+        'img/shining_eye.png',
+        'img/gold_pile_16.png',
+        'img/chest_2_open.png',
+        'img/hippogriff_old.png',
+        'img/ozocubus_refrigeration.png'      
+      })
+    end
+       
+end
+
+require('dungeon_source')
